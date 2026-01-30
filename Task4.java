d09b84cdda54efe1862a99f516d6e74eea992bc7
b-prasanth9 <r02737758@gmail.com>
2026-01-30 22:03:08 +0530

    Add files via upload

---
+++ Task4.java
@@ -0,0 +1,17 @@
+
+public class Task4 {
+    public static void main(String[] args) {
+
+        int[] arr = {12, 45, 7, 89, 34};
+
+        int max = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        System.out.println(max);
+    }
+}
